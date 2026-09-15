61d1d19c1443f78a4310ec24b215eb275f8c7af7
sarvas477-pixel <sarvas477@gmail.com>
2026-09-15 11:10:54 +0000

    validate

--- src/main.py
+++ src/main.py
@@ -1,15 +1,8 @@
 import csv
 
+
 with open("data/invoices.csv", "r", newline="") as file:
     reader = csv.DictReader(file)
 
     for invoice in reader:
         print(invoice)
-
-    amount = 3000
-    limit = 5000
-
-    if amount > limit :
-        print("FLAGEED: amount exceeds limit")
-    else :
-        print ("Clean: amount is within limit")
